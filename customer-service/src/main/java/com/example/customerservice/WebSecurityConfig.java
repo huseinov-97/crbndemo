a03bd2d7506bcc35483fb4ed9d1f1e4af8a3d831
huseinov-97 <mhuseinov7@gmail.com>
2021-04-26 01:32:10 +0400

    second

--- customer-service/src/main/java/com/example/customerservice/WebSecurityConfig.java
+++ customer-service/src/main/java/com/example/customerservice/WebSecurityConfig.java
@@ -1,13 +1,27 @@
 package com.example.customerservice;
 
+import com.example.customerservice.service.MyUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private MyUserDetailsService myUserDetailsService;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        
+        auth.userDetailsService(myUserDetailsService);
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 }
